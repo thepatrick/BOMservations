a0be04dea180ae0e846797d3e87fd102d64846cd
Patrick Quinn-Graham <make-contact@pftqg.com>
2011-02-20 11:16:16 +1100

    Initial Commit

---
+++ BOMServations/BOMServationsAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BOMServationsAppDelegate.h
+//  BOMServations
+//
+//  Created by Patrick Quinn-Graham on 20/02/11.
+//  Copyright 2011 Sharkey Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BOMServationsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ BOMServations/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BOMServations
+//
+//  Created by Patrick Quinn-Graham on 20/02/11.
+//  Copyright 2011 Sharkey Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
